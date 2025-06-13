def918c0dc8670a666a0bfb0b5e162a86b5a8a2b
Ryan Emerson <remerson@redhat.com>
2025-06-13 09:44:49 +0100

    Revert "Ensure that num_owners=2 is set on session caches with volatilesessions profile"
    This reverts commit c9dfb597ef6cd6916bb8bdbcaaa77aa02cecb7a1.
    
    Signed-off-by: Ryan Emerson <remerson@redhat.com>

--- testsuite/model/src/test/java/org/keycloak/testsuite/model/parameters/VolatileUserSessions.java
+++ testsuite/model/src/test/java/org/keycloak/testsuite/model/parameters/VolatileUserSessions.java
@@ -18,8 +18,6 @@ package org.keycloak.testsuite.model.parameters;
 
 import org.keycloak.common.Profile;
 import org.keycloak.common.profile.PropertiesProfileConfigResolver;
-import org.keycloak.spi.infinispan.CacheEmbeddedConfigProviderSpi;
-import org.keycloak.spi.infinispan.impl.embedded.DefaultCacheEmbeddedConfigProviderFactory;
 import org.keycloak.testsuite.model.Config;
 import org.keycloak.testsuite.model.KeycloakModelParameters;
 
@@ -34,9 +32,6 @@ public class VolatileUserSessions extends KeycloakModelParameters {
     @Override
     public void updateConfig(Config cf) {
         updateConfigForJpa(cf);
-        cf.spi(CacheEmbeddedConfigProviderSpi.SPI_NAME)
-              .provider(DefaultCacheEmbeddedConfigProviderFactory.PROVIDER_ID)
-              .config(DefaultCacheEmbeddedConfigProviderFactory.CONFIG, "test-ispn-volatile-sessions.xml");
     }
 
     public static void updateConfigForJpa(Config cf) {
