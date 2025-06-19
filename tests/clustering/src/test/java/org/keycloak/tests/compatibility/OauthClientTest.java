ba96b908105fb1078bf8fcdad94c79c3fb8b3b65
Ryan Emerson <remerson@redhat.com>
2025-06-19 10:32:04 +0100

    Simplify tests by using default OAuthClient

--- tests/clustering/src/test/java/org/keycloak/tests/compatibility/OauthClientTest.java
+++ tests/clustering/src/test/java/org/keycloak/tests/compatibility/OauthClientTest.java
@@ -17,78 +17,37 @@
 
 package org.keycloak.tests.compatibility;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.keycloak.representations.idm.ClientRepresentation;
-import org.keycloak.testframework.annotations.InjectHttpClient;
 import org.keycloak.testframework.annotations.InjectLoadBalancer;
-import org.keycloak.testframework.annotations.InjectRealm;
 import org.keycloak.testframework.annotations.InjectUser;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
 import org.keycloak.testframework.clustering.LoadBalancer;
-import org.keycloak.testframework.oauth.DefaultOAuthClientConfiguration;
 import org.keycloak.testframework.oauth.OAuthClient;
-import org.keycloak.testframework.oauth.TestApp;
-import org.keycloak.testframework.oauth.annotations.InjectTestApp;
-import org.keycloak.testframework.realm.ClientConfigBuilder;
-import org.keycloak.testframework.realm.ManagedRealm;
+import org.keycloak.testframework.oauth.annotations.InjectOAuthClient;
 import org.keycloak.testframework.realm.ManagedUser;
-import org.keycloak.testframework.ui.annotations.InjectWebDriver;
-import org.keycloak.testframework.util.ApiUtil;
 import org.keycloak.testsuite.util.oauth.AuthorizationEndpointResponse;
-import org.openqa.selenium.WebDriver;
 
 @KeycloakIntegrationTest
 public class OauthClientTest {
 
-    @InjectLoadBalancer
-    LoadBalancer loadBalancer;
-
-    @InjectHttpClient
-    HttpClient httpClient;
-
-    @InjectRealm
-    ManagedRealm realm;
-
     @InjectUser(config = OAuthUserConfig.class)
     ManagedUser user;
 
-    @InjectTestApp
-    TestApp testApp;
+    @InjectLoadBalancer
+    LoadBalancer loadBalancer;
 
-    @InjectWebDriver
-    WebDriver webDriver;
+    @InjectOAuthClient
+    OAuthClient oAuthClient;
 
     @Test
     public void testLoginLogout() {
-        AuthorizationEndpointResponse response = oauth(0).doLogin(user.getUsername(), user.getPassword());
+        oAuthClient.baseUrl(loadBalancer.node(0).getBase());
+        AuthorizationEndpointResponse response = oAuthClient.doLogin(user.getUsername(), user.getPassword());
         Assertions.assertTrue(response.isRedirected());
 
-        String idTokenHint = oauth(1).doAccessTokenRequest(response.getCode()).getIdToken();
-        oauth(1).logoutForm().idTokenHint(idTokenHint).open();
-    }
-
-    // TODO replace with just setting baseUrl on OAuthClient?
-    private OAuthClient oauth(int index) {
-        String baseUrl = loadBalancer.node(index).getBase();
-
-        String redirectUri = testApp.getRedirectionUri();
-        ClientRepresentation testAppClient = new DefaultOAuthClientConfiguration().configure(ClientConfigBuilder.create())
-              .redirectUris(redirectUri)
-              .build();
-
-        String clientId = testAppClient.getClientId();
-        String clientSecret = testAppClient.getSecret();
-        ApiUtil.handleCreatedResponse(realm.admin().clients().create(testAppClient));
-
-        OAuthClient oAuthClient = new OAuthClient(baseUrl, (CloseableHttpClient) httpClient, webDriver);
-        oAuthClient.config()
-              .realm(realm.getName())
-              .client(clientId, clientSecret)
-              .redirectUri(redirectUri)
-              .scope("openid profile");
-        return oAuthClient;
+        oAuthClient.baseUrl(loadBalancer.node(1).getBase());
+        String idTokenHint = oAuthClient.doAccessTokenRequest(response.getCode()).getIdToken();
+        oAuthClient.logoutForm().idTokenHint(idTokenHint).open();
     }
 }
